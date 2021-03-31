6745d64174546f8f59392c1c06d7d328a9367ee3
Malva2312 <you@example.com>
2021-03-31 15:24:31 +0100

    Malva: Comecei a escrever as funçoes principais no sentido descendente

--- PROG_T4_03_/PROG_T4_03_.cpp
+++ PROG_T4_03_/PROG_T4_03_.cpp
@@ -1,20 +1,60 @@
-// PROG_T4_03_.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <string.h>
+using namespace std; // makes all the code more readable
 
-int main()
-{
-    std::cout << "O Malva esteve aqui!!";
+void menu(string &inst) {        // menu function
+
+    cout << "Menu \nPlease choose an option" << endl;
+    cout << "1) Rules \n2) Play \n 0) Exit" << endl;
+    cin >> inst;    //need to check input
+    cout << endl;
+
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
 
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
+/* Options function ################################################ */
+void showRules() {
+    // inserir as regras deste jogo 
+    cout << "Rules: \n\n" << "Não sei o que dizer nas regras amigo Dinis!!" << endl; 
+}
+
+void play() {
+    int N_MAZE;
+    cout << "Pick game Maze" << endl;
+    cin >> N_MAZE;                        //need to check input
+
+
+}
+void exit() {}
+/* ################################################################# */
+
+
+void readInst(string& inst) {           // funcion that reads intruction for the nex iteration of the loop
+    cout << "Next instruction" << endl; // standart text for read instruction, we must change that
+    cin >> inst;                        //need to check input
+    cout << endl;
+}
+
+
+int main()   //main function
+{
+    bool running = true;
+    string game_state = "menu", Inst;
+
+    cout << "Welcome to this game names <inserir nome deste jogo>" << endl << endl; //need to check game's name
+
+    while (running) { // game main loop
+        
+        if (game_state == "menu") { menu(Inst); }
+
+        else if (game_state == "rules") { showRules(); }
+
+        else if (game_state == "play") { play(); }
+
+        else if (game_state == "exit") { exit(); }
+
+
+        readInst(Inst);
+    }
+}
+
